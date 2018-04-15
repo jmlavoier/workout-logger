8fc288642b2e19b30921cc8d27c8725725434ee5
Joao Lavoier <jmlavoier@hotmail.com>
2018-04-15 16:49:29 -0300

    Init develop environment with webpack, react and css modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './css/main.sass';
+
+render(
+  <div>
+    <h1>Hello Magalu!</h1>
+    <p>Starting project</p>
+  </div>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpackRules = require('./webpack.rules');
+
+const PATHS = {
+  root: path.resolve(__dirname, 'build'),
+  build: path.resolve(__dirname, 'build'),
+  src: path.resolve(__dirname, 'src')
+}
+
+module.exports = {
+  entry: PATHS.src,
+  resolve: {
+    modules: [
+      path.resolve(PATHS.src),
+      path.resolve('./node_modules')
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: PATHS.build
+  },
+  devServer: {
+    contentBase: PATHS.root,
+    compress: true,
+    port: 8888
+  },
+  module: {
+    rules: webpackRules
+  }
+};

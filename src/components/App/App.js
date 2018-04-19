ddf28ab0582e0f47217bc4fa17afe5ba34c9dd9c
Joao Lavoier <jmlavoier@hotmail.com>
2018-04-19 04:33:16 -0300

    Fix Box export

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Form from 'containers/Form';
+
 const App = () => (
   <div>
     <h1>Hello Magalu!</h1>
     <p>Starting project</p>
+    <Form />
   </div>
 );
 

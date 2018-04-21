0216b5e5cad0c20c31b1bcb927c58e965fc652f2
Joao Lavoier <jmlavoier@hotmail.com>
2018-04-20 22:09:26 -0300

    Fix App component test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from 'store';
 import { shallow, mount } from 'enzyme';
 
 import App from './App';
 
 describe('<App/>', () => {
   it('Should component match snapshot', () => {
-    const wrapper = mount(<App/>);
+    const wrapper = mount(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    );
   
     expect(wrapper).toMatchSnapshot();
   });

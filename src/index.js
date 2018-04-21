fa62e0eac7e2ec38753dc87c058668544bc781b8
Joao Lavoier <jmlavoier@hotmail.com>
2018-04-21 09:35:19 -0300

    Implement redux persist (localStorage)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './components/App';
-import store from './store';
+import store, { persistor } from './store';
 
 
 import './css/main.sass';
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 import { combineReducers, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
 import form from 'containers/Form/redux/reducer';
 import workoutList from 'containers/WorkoutList/redux/reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducers = combineReducers({
   form,
   workoutList,
 });
 
-const store = createStore(rootReducers);
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
 
 export default store;

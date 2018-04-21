21b1c8c3323dd256b65e3e4f0b62d89d0f9c581e
Joao Lavoier <jmlavoier@hotmail.com>
2018-04-20 22:13:43 -0300

    Refactor Form container

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uid from 'uid';
 
 import Box from 'components/Box';
 import Container from 'components/Container';
@@ -7,35 +8,48 @@ import InputTime from 'components/InputTime';
 import Button from 'components/Button';
 import Select from 'components/Select';
 
-const optionsMock = [
+const workoutTypeOptions = [
   { id: '1', value: 'Run' },
   { id: '2', value: 'Swimming' },
+  { id: '3', value: 'Bike' },
+  { id: '4', value: 'Stretch' },
+  { id: '5', value: 'Yoga' },
+  { id: '6', value: 'Gym' },
 ];
 
 const handleChange = (changeField, fieldName) => (e) => {
   changeField(fieldName, e.target.value);
 };
 
-const Form = ({ form, changeField }) => (
-  <Container>
-    <Box>
-      <InputTime name="time-spent" value={form.timeSpent} onChange={handleChange(changeField, 'timeSpent')} />
-    </Box>
-    <Box>
-      <Select name="workout-type" options={optionsMock} value={form.workoutType} onChange={handleChange(changeField, 'workoutType')} />
-    </Box>
-    <Box>
-      <InputTime name="date" value={form.date} onChange={handleChange(changeField, 'date')} />
-    </Box>
-    <Box>
-      <Button onClick={() => {}} >Add</Button>
-    </Box>
-  </Container>
-);
+const handleClick = (clickAdd, form) => () => {
+  clickAdd(form, uid());
+};
+
+const Form = ({ form, changeField, clickAdd }) => {
+  const { timeSpent, workoutType, date } = form;
+
+  return (
+    <Container>
+      <Box>
+        <InputTime name="time-spent" value={timeSpent} onChange={handleChange(changeField, 'timeSpent')} />
+      </Box>
+      <Box>
+        <Select name="workout-type" options={workoutTypeOptions} value={workoutType} onChange={handleChange(changeField, 'workoutType')} />
+      </Box>
+      <Box>
+        <InputTime name="date" value={date} onChange={handleChange(changeField, 'date')} />
+      </Box>
+      <Box>
+        <Button onClick={handleClick(clickAdd, form)} >Add</Button>
+      </Box>
+    </Container>
+  );
+};
 
 Form.propTypes = {
   form: PropTypes.objectOf(PropTypes.string).isRequired,
   changeField: PropTypes.func.isRequired,
+  clickAdd: PropTypes.func.isRequired,
 };
 
 export default Form;
--- src/containers/Form/container.js
+++ src/containers/Form/container.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
-import Form from './Form';
 
+import { clickAdd } from 'containers/WorkoutList/redux/actions';
 import { changeField } from './redux/actions';
+import Form from './Form';
 
 const mapStateToProps = ({ form }) => ({ form });
 
 const mapDispatchToProps = dispatch => ({
   changeField: (fieldName, value) => dispatch(changeField(fieldName, value)),
+  clickAdd: (form, id) => dispatch(clickAdd(form, id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form);
